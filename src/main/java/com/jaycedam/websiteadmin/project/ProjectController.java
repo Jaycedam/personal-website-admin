9a81c188a45cedd7b3f47b02aaf563015a114563
Jordan <jaycedam@gmail.com>
2022-04-27 16:12:19 -0400

    added CrossOrigin to API

--- src/main/java/com/jaycedam/websiteadmin/project/ProjectController.java
+++ src/main/java/com/jaycedam/websiteadmin/project/ProjectController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/projects")
+@CrossOrigin("*")
 public class ProjectController {
 
     private final ProjectService projectService;
